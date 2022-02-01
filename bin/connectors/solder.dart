5c8a71ed13e03dda1cf6b60a781c8f056a946d55
Ali <ali.akber@brainstation-23.com>
2022-02-01 23:51:50 +0600

    refactor code

--- bin/connectors/solder.dart
+++ bin/connectors/solder.dart
@@ -4,15 +4,25 @@ import '../components/power_component.dart';
 class Solder{
   static connect(Component c1, Component c2){
     if(c1 is PowerComponent) {
-      c1.pin1.connectionStream.listen((event) {
-        print("emitted value is $event");
-        c2.pin2.emitConnection(event);
-      });
+      connectPowerSourceWithComponent(c1, c2);
     }
     else if(c2 is PowerComponent){
-      c2.pin2.connectionStream.listen((event) {
-        c1.pin1.emitConnection(event);
-      });
+      connectComponentWithPowerSource(c1, c2);
     }
   }
+
+  static connectPowerSourceWithComponent(PowerComponent p, Component c){
+    p.pin1.connectionStream.listen((event) {
+      print("emitted value is $event");
+      c.pin2.emitConnection(event);
+    });
+  }
+
+  static connectComponentWithPowerSource(Component c, PowerComponent p){
+    p.pin2.connectionStream.listen((event) {
+      print("emitted value is $event");
+      c.pin1.emitConnection(event);
+    });
+  }
+
 }
